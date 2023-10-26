272aa167d4387b8ce16f2025287bfaf462d5f063
jmejiamu <jmejiamu@gmail.com>
2023-10-26 13:34:42 -0700

    JWT-#13

---
+++ src/utils/auth.ts
@@ -0,0 +1,55 @@
+import jwt from "jsonwebtoken";
+import { HttpStatusCode } from "./status-code";
+import bcrypt from "bcrypt";
+
+interface IUser {
+  id: number;
+  username: string;
+}
+
+export const comparePassword = (password: string, hash: string) => {
+  return bcrypt.compareSync(password, hash);
+};
+
+export const hashPassword = (password: string) => {
+  const salt = bcrypt.genSaltSync(10);
+  return bcrypt.hash(password, salt);
+};
+export const createJWT = (user: IUser) => {
+  const secretJWT = process.env.JWT_SECRET as string;
+  const token = jwt.sign(
+    {
+      id: user.id,
+      username: user.username,
+    },
+    secretJWT
+  );
+  return token;
+};
+
+export const protect = (req: any, res: any, next: any) => {
+  const bearer = req.headers.authorization;
+  if (!bearer) {
+    return res
+      .status(HttpStatusCode.UNAUTHORIZED)
+      .json({ message: "You need to login" });
+  }
+
+  const [, token] = bearer.split(" ");
+  if (!token) {
+    return res
+      .status(HttpStatusCode.UNAUTHORIZED)
+      .json({ message: "Not valid token" });
+  }
+  try {
+    const secretJWT = process.env.JWT_SECRET as string;
+    const decoded = jwt.verify(token, secretJWT);
+    // This may need to change
+    req.body.user = decoded;
+    next();
+  } catch (error) {
+    return res
+      .status(401)
+      .json({ message: "Invalid token - You need to login" });
+  }
+};
